b564da2757b1ecebd341437014118a88d1d9bc9e
Evgeniy Muravetskiy <Ekiruui@gmail.com>
2023-02-26 01:50:05 +0300

    fix

--- server/src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
