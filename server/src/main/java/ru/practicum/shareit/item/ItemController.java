7e39c7f161d38748887f5a44b9188a02e1dddb38
Evgeniy Muravetskiy <Ekiruui@gmail.com>
2023-02-26 02:17:16 +0300

    fix

--- server/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -8,6 +8,7 @@ import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemFullDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.service.ItemService;
 
 import java.util.Collection;
 
